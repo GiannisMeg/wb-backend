7b992cbe5d15084171c708cb203dd5751a51a618
JS1209 <j.a.schoenmakers@outlook.com>
2022-07-20 17:22:55 +0200

    Login basically works; it sends back a token. Now we must decide how to respond.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const productRouter = require("./routers/product");
 const categoryRouter = require("./routers/category");
+const userRouter = require("./routers/user");
 
 const app = express();
 const PORT = 4000;
@@ -10,5 +11,6 @@ app.use(cors());
 
 app.use("/products", productRouter);
 app.use("/categories", categoryRouter);
+app.use("/users", userRouter);
 
 app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
